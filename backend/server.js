b9f044e9876606cb93fb7c82cd5971331578996f
Charlesiubao <kristoffer.charles.reyes@gmail.com>
2021-04-06 16:57:22 -0700

    begginings

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,51 @@ app.use(require('cors')())
 
 const models = require('./models')
 
+const createUser = async (req,res) => {
+  try {
+    console.log(req.body)
+    const user = await models.user.create({
+      email: req.body.email,
+      password: req.body.password
+    })
+    console.log(user)
+    res.json({ message: 'okidokey', user })
+  } catch (error) {
+    res.status(400)
+    res.json({ error: 'email already taken'})
+  }
+}
+app.post('/users', createUser)
+
+const login = async (req,res) => {
+  try {
+    const user = await models.user.findOne({
+      where: {
+        email: req.body.email
+      }
+    })
+
+    console.log(user)
+
+    if (user.password === req.body.password) {
+      res.json({ message: 'login successful', user: user})
+    } else {
+      res.status(401)
+      res.json({ error: 'login failed'})
+    }
+    console.log(user.password)
+    console.log(req.body.password)
+  } catch (error) {
+    res.status(400)
+      res.json({ error: 'login failed'})
+  }
+}
+app.post('/users/login', login)
+
+
+// const lookEmUp = 
+
+
 const PORT = process.env.port || 3001
 app.listen(PORT, () => {
   routesReport.print()
--- frontend/main.js
+++ frontend/main.js
@@ -23,3 +23,69 @@ document.querySelector('#profile-link').addEventListener('click', () => {
   document.querySelectorAll('section').forEach(s => s.classList.add('hidden'))
   document.querySelector('#profile-content').classList.remove('hidden')
 })
+
+//form submission
+
+document.querySelector('#signup-form').addEventListener('submit', async (event) => {
+  event.preventDefault()
+
+  const email = document.querySelector('#signup-email').value
+  const password = document.querySelector('#signup-password').value
+
+  try {
+    const response = await axios.post('http://localhost:3001/users', {
+    email: email,
+    password: password
+  })
+  console.log(response)
+  } catch (error) {
+    console.log(error)
+    alert('cannot login')
+  }
+  // make a request
+  // POST http://localhost:3001/users
+  // send a body
+  // email and password
+  // get em from the inputs use .value
+})
+
+document.querySelector('#login-form').addEventListener('submit', async (event) => {
+  event.preventDefault()
+
+  const email = document.querySelector('#login-email').value
+  const password = document.querySelector('#login-password').value
+
+  try {
+    const response = await axios.post('http://localhost:3001/users/login', {
+    email: email,
+    password: password
+  })
+  console.log(response)
+
+  const userId = response.data.user.userId
+  localStorage.setItem('userId', userId)
+
+  document.querySelector('#login-link').classList.add('hidden')
+  document.querySelector('#signup-link').classList.add('hidden')
+  document.querySelector('#logout-link').classList.remove('hidden')
+  document.querySelector('#profile-link').classList.remove('hidden')
+
+  document.querySelectorAll('section').forEach(s => s.classList.add('hidden'))
+
+
+  } catch (error) {
+    console.log(error)
+    alert('login failed')
+  }
+})
+
+if (localStorage.getItem('userId')) {
+  document.querySelector('#signup-link').classList.add('hidden')
+  document.querySelector('#login-link').classList.add('hidden')
+} else {
+
+  document.querySelector('#logout-link').classList.add('hidden')
+  document.querySelector('#profile-link').classList.add('hidden')
+}
+
+
