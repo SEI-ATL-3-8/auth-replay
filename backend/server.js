655d8f3152af960aa76ee6399924fb11af34261e
Anel Tuyebayeva <aneltuyebayeva@Anels-MacBook-Pro.local>
2021-04-06 21:24:16 -0400

    setup, chapter 1-3

--- backend/server.js
+++ backend/server.js
@@ -14,3 +14,37 @@ app.listen(PORT, () => {
   routesReport.print()
 })
 
+const createUser = async(req, res) => {
+  try {
+    let user = await models.user.create ({
+    email: req.body.email,
+    password: req.body.password
+  })
+    res.json({message: 'ok', user})
+  } catch (error) {
+    res.status(400)
+    res.json({error: 'email already taken'})
+  }
+}
+app.post('/users', createUser)
+
+const login = async (req,res) => {
+  try {
+    const user = await models.user.findOne ({
+      where: {
+        email: req.body.email
+      }
+    })
+
+    if (user.password === req.body.password) {
+      res.json({message: 'login successful', user: user})
+    } else {
+      res.status(401)
+      res.json ({error: 'login failed, password is incorrect'})
+    }
+  } catch (error) {
+      res.status(400)
+      res.json ({error: 'login failed'})
+  }
+}
+app.post('/users/login', login)
