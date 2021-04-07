5b84692ba03d1963bf1f2dddfb8e58c33aeb65f0
Jason Ouyang <jasonouyang112@gmail.com>
2021-04-06 21:39:54 -0700

    chapter 4

--- frontend/main.js
+++ frontend/main.js
@@ -1,3 +1,4 @@
+
 // nav links
 document.querySelector('#home-link').addEventListener('click', () => {
   document.querySelectorAll('section').forEach(s => s.classList.add('hidden'))
@@ -12,11 +13,15 @@ document.querySelector('#signup-link').addEventListener('click', () => {
 document.querySelector('#login-link').addEventListener('click', () => {
   document.querySelectorAll('section').forEach(s => s.classList.add('hidden'))
   document.querySelector('#login-content').classList.remove('hidden')
+ 
 })
 
 document.querySelector('#logout-link').addEventListener('click', () => {
   document.querySelectorAll('section').forEach(s => s.classList.add('hidden'))
-  document.querySelector('#home-content').classList.remove('hidden')
+  document.querySelector('#home-content').classList.remove('hidden');
+  localStorage.removeItem('userId');
+  authenticate();
+
 })
 
 document.querySelector('#profile-link').addEventListener('click', () => {
@@ -48,9 +53,15 @@ document.getElementById('signup-form').addEventListener('submit', async event =>
 
     try {
       const response = await axios.post('http://localhost:3001/users', formParams);
-      const userId = response.data.user.id;
+      if (response.data.message === 'success') {
+        const userId = response.data.user.id;
     
-      localStorage.setItem('userId', userId);
+        localStorage.setItem('userId', userId);
+        authenticate();
+        document.querySelectorAll('section').forEach(s => s.classList.add('hidden'))
+        document.querySelector('#home-content').classList.remove('hidden')
+      }
+ 
     }
     catch({response}) {
       showErrorMessage(response.data.error);
@@ -58,7 +69,58 @@ document.getElementById('signup-form').addEventListener('submit', async event =>
 });
 
 // Form Event Listener Login Account
+document.getElementById('login-form').addEventListener('submit', async event => {
+  event.preventDefault();
 
-const authenticate = () => {
-  
+  const formParams = {
+    email : event.target.elements[0].value,
+    password: event.target.elements[1].value
+  };
+
+  try {
+    const response = await axios.post('http://localhost:3001/users/login', formParams);
+    if (response.data.message === 'success') {
+      const userId = response.data.user.id;
+      localStorage.setItem('userId', userId);
+      authenticate();
+      document.querySelectorAll('section').forEach(s => s.classList.add('hidden'))
+      document.querySelector('#home-content').classList.remove('hidden')
+    }
+
+
+  }
+  catch({response}) {
+    showErrorMessage(response.data.error);
+  }
+
+});
+
+// Event Listener for Logout Button
+
+
+const authenticate = async () => {
+    
+    if (localStorage.getItem('userId')) {
+      try {
+        const response = await axios.get('http://localhost:3001/users/verify', {
+            params: {id: localStorage.getItem('userId') }
+        });
+
+        if (response.data.message === 'success') {
+          document.querySelector('#login-link').classList.add('hidden');
+          document.querySelector('#signup-link').classList.add('hidden');
+        }
+
+      }
+      catch({response}) {
+        showErrorMessage(response.data.error);
+      }
+    }
+
+    else {
+      document.querySelector('#login-link').classList.remove('hidden');
+      document.querySelector('#signup-link').classList.remove('hidden');
+    }
 }
+
+authenticate();
