c35658bdd32ac9c103ba15be156ae742fe1d4a91
Marquette Brown <marcstroy1@gmail.com>
2021-04-07 10:23:42 -0400

    help

--- backend/server.js
+++ backend/server.js
@@ -14,3 +14,35 @@ app.listen(PORT, () => {
   routesReport.print()
 })
 
+
+const newUser = async(req, res) => {
+  try {
+      let user = await models.user.create({
+
+          user: req.body.user,
+          password: req.body.password
+
+      })
+      res.json({ message: 'Hello new user', user })
+  } catch (error) {
+      res.json({ error: 'email taken, try again' })
+
+  }
+}
+
+
+const newLogin = async(req, res) => {
+  try {
+      const login = await models.user.findOne({
+          where: { email: req.body.email }
+      })
+      if (user.password === req.body.password) {
+          res.json({ message: 'login success', login })
+      } else { res.status(401).json({ error }) }
+  } catch (error) {
+      res.status(400).json({ error: 'login unsucccesful' })
+  }
+}
+app.post('/user/login', newLogin)
+app.post('/user', newUser)
+
