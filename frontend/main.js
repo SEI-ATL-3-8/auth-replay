9db8b881f7c0933737a6307482360dc4f07e5105
Derek Ensign <derekensign@gmail.com>
2021-04-06 20:29:39 -0500

    set up through logout

--- frontend/main.js
+++ frontend/main.js
@@ -23,3 +23,82 @@ document.querySelector('#profile-link').addEventListener('click', () => {
   document.querySelectorAll('section').forEach(s => s.classList.add('hidden'))
   document.querySelector('#profile-content').classList.remove('hidden')
 })
+
+document.querySelector('#signup-form').addEventListener('submit', async (event) => {
+  // make a request, POST http://localhost:3001/users, send a body
+  event.preventDefault()
+
+  const email = document.querySelector('#signup-email').value 
+  const password = document.querySelector('#signup-password'). value
+
+  try {
+  const response = await axios.post('http://localhost:3001/users', {
+    email: email,
+    password: password
+  })
+  const userId = response.data.user.id
+  localStorage.setItem('userId', userId)
+
+} catch (error) {
+  console.log(error)
+  console.log('Email already taken')
+}
+})
+
+document.querySelector('#login-form').addEventListener('submit', async (event) => {
+  // make a request, POST http://localhost:3001/users, send a body
+  event.preventDefault()
+
+  const email = document.querySelector('#login-email').value 
+  const password = document.querySelector('#login-password'). value
+
+  try {
+  const response = await axios.post('http://localhost:3001/users/login', {
+    email: email,
+    password: password
+  })
+  const userId = response.data.user.id
+  localStorage.setItem('userId', userId)
+
+  document.querySelectorAll('section').forEach(s =>
+    s.classList.add('hidden'))
+
+  document.querySelector('#home-content').classList.remove('hidden')
+
+  document.querySelector('#login-link').classList.add('hidden')
+  document.querySelector('#signup-link').classList.add('hidden')
+  document.querySelector('#profile-link').classList.remove('hidden')
+  document.querySelector('#logout-link').classList.remove('hidden')
+
+} catch (error) {
+  console.log(error)
+  console.log('login failed')
+}
+})
+
+document.querySelector('#logout-link').addEventListener('click', () => {
+  document.querySelectorAll('section').forEach(s =>
+    s.classList.add('hidden'))
+
+    document.querySelector('#home-content').classList.remove('hidden')
+
+    localStorage.removeItem('userId')
+    
+    document.querySelector('#login-link').classList.remove('hidden')
+    document.querySelector('#signup-link').classList.remove('hidden')
+    document.querySelector('#profile-link').classList.add('hidden')
+    document.querySelector('#logout-link').classList.add('hidden')
+    
+  })
+
+
+//as soon as page loads, see if someone is logged in or not
+
+if ( localStorage.getItem('userId')) {
+  document.querySelector('#signup-link').classList.add('hidden')
+  document.querySelector('#login-link').classList.add('hidden')
+}
+else {
+  document.querySelector('#logout-link').classList.add('hidden')
+  document.querySelector('#profile-link').classList.add('hidden')
+  }
