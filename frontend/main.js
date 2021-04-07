33339cf6c5ebae56811a167b127bea7659009b72
parksungchul <parksungchul@Sungchul-MacBook-Pro.local>
2021-04-06 22:58:43 -0400

    chapter 1 done

--- frontend/main.js
+++ frontend/main.js
@@ -23,3 +23,27 @@ document.querySelector('#profile-link').addEventListener('click', () => {
   document.querySelectorAll('section').forEach(s => s.classList.add('hidden'))
   document.querySelector('#profile-content').classList.remove('hidden')
 })
+
+//chapter 1
+document.querySelector('#signup-form').addEventListener('submit', async (event)=> {
+  event.preventDefault()
+  const email = document.querySelector('#signup-email').value
+  const password = document.querySelector('#signup-password').value
+  // console.log(email)
+  // console.log(password)
+  try {
+    const response = await axios.post('http://localhost:3001/users', {
+      email: email,
+      password: password
+    })
+    console.log(response)
+
+    const userId = response.data.user.id
+    localStorage.setItem('userId', userId)
+
+  } catch (error) {
+    console.log(error)
+    alert('email is already taken')
+  }
+})
+
